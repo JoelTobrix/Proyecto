00cc3238d4347d73fd0f23e3a8fd9307101c62ee
JoelTobrix <156262231+JoelTobrix@users.noreply.github.com>
2024-02-21 01:02:04 -0500

    Add files via upload

---
+++ proyect/Registro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro</title>
+  
+    <!-- Incluye el archivo CSS -->
+    <link rel="stylesheet" href="modelo.css">
+    
+</head>
+<body>
+<form class="login-form" action="#" method="post"> 
+<div class="dark-overlay">
+ <center> <img src="https://us.123rf.com/450wm/tifani1/tifani11801/tifani1180100032/93016694-usuario-icono-de-ilustraci%C3%B3n-vectorial-sobre-fondo-negro.jpg" width=200px; height= 200px;> </center>
+              <span class="fa-stack fa-lg">
+                <i class="fa fa-circle fa-stack-2x"></i>
+                <i class="fa fa-lock fa-stack-1x"></i>
+            </span>
+            </p>
+     <input type="email" class="login-username" autofocus required placeholder="Email" />
+     <input type="password" class="login-password" required placeholder="Password">
+     <input type="submit" value="Registrar Ahora">
+</div>
+</body>
+</html>
